aadb89c4e13d6bd41c047a13752fc2e432917572
Krishprajapati15 <prajapatikrish132005@gmail.com>
2025-05-01 19:48:17 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,250 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  {
+    name: "Resume",
+    link: "https://drive.google.com/drive/folders/1wpl4WyZ-dsjRoiqk9ix3ntnQHXVVpAhG",
+  },
+  { name: "Projects", link: "#projects" },
+
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "I deliver scalable and efficient solutions with a focus on user-centric design.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Turning complex problems into elegant solutions.",
+    description: "",
+    className:
+      "lg:col-span-2 md:col-span-3 md:row-span-2 min-h-[40vh] select-none",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Hire me",
+    description: "Are you hiring? Check out ",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2 flex",
+    imgClassName: "",
+    titleClassName: "justify-end",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Innovative developer crafting seamless digital experiences.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Crafting Clean Code with Modern Design Principles",
+    description: "Passionate about transforming ideas into reality.",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Ready to bring your ideas to life?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Xenesis-2025",
+    des: "As a Team Lead for Xenesis 2025, my university's biggest tech fest, I contributed to its success. Check out the official website: [Xenesis 2025](https://www.xenesis2025.com).",
+    img: "/xenesis.png",
+    iconLists: ["/php.png", "/tail.svg", "/js.svg", "/gsap.svg", "/mysql.png"],
+    link: "https://xenesis.ldrp.ac.in/",
+  },
+  {
+    id: 2,
+    title: "WealthLens",
+    des: "AI-powered platform for tracking income, expenses, and taxes with features like SmartPaySync and Credit Card Track AI. 💸",
+    img: "/wealthlens.png",
+    iconLists: [
+      "/next.svg",
+      "/re.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/openai.png",
+      "/mongo.svg",
+    ],
+    link: "https://github.com/Krishprajapati15/WealthLens",
+  },
+  {
+    id: 3,
+    title: "HackWatch",
+    des: "HackerWatch: AI-powered cybersecurity for real-time threat detection and proactive digital protection. 🔒🚀",
+    img: "/hackwatch.png",
+    iconLists: [
+      "/re.svg",
+      "/css.svg",
+      "/js.svg",
+      "/chartjs.svg",
+      "/fm.svg",
+      "/mui.svg",
+      "/python.png",
+    ],
+    link: "https://github.com/Krishprajapati15/Hackwatch",
+  },
+
+  {
+    id: 4,
+    title: "zapconvert",
+    des: "ZapConvert: A versatile platform to convert music, files, images, and videos into various formats effortlessly. 🌟",
+    img: "/zapconvert.png",
+    iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg", "/mongo.svg"],
+    link: "https://www.zapconvert.me/",
+  },
+  {
+    id: 5,
+    title: "SecureShare",
+    des: "SecureShare: Privacy-first platform for sharing files via secure, unique links without local storage. 🔒📂",
+    img: "/file.png",
+    iconLists: ["/node.png", "/re.svg", "/tail.svg", "/js.svg", "/mongo.svg"],
+    link: "https://www.zapconvert.me/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "ZapConvert",
+    desc: "Developed ZapConvert, a user-friendly web application for seamless file format conversions, supporting files, images, videos, music, and more in different formats. It features a clean white-themed interface and a custom animated navbar.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Project Lead – Xenesis 2025",
+    desc: "Led the development of the official Xenesis 2025(LDRP College's Biggest Tech Fest) website, managing multiple panels including User, Admin, Volunteer, and Food Coupon Scanning systems, ensuring a seamless experience for over 5,000 participants.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Project",
+    desc: "Developed a web application for a client, handling both design and implementation using Mern Stack.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Team Lead in Hackathon",
+    desc: "Led teams in multiple hackathons, including 5 finals and national-level events like SBI Hack-AI-Thon (Finalist) and SIH 2024 Grand Finale (Waitlist), with a focus on MERN stack projects.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    href: "https://github.com/krishprajapati15",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    href: "https://x.com/krishPr88603152?t=1Zjwv7rjWWgzITPBQe_KRg&s=08 ",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    href: "https://www.linkedin.com/in/krish-prajapati-37417226a/",
+  },
+];
