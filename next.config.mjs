a112d31dbc25952231d62801f192d1fc18f9b4c1
Krishprajapati15 <prajapatikrish132005@gmail.com>
2025-05-01 20:06:11 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true, // Optional: Enable experimental features
+  },
+  output: "export", // Static Export (alternative)
+};
 
 export default nextConfig;
